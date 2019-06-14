7024c2b72e270df675fc9eb7e167935318be4bb0
lcodo <laia.codo@bsc.es>
2019-06-14 13:05:56 +0200

    add disclaimer for add blocked browsers

--- public/home/redirect.php
+++ public/home/redirect.php
@@ -8,6 +8,7 @@
 </head>
 
 <body>
+    <p>Application not correctly be loaded. Please, check your advertisement blocker set up</p>
     <input type="hidden" id="base-url" value="<?php echo $GLOBALS['BASEURL']; ?>" />
     <script src="assets/global/plugins/jquery.min.js" type="text/javascript"></script>
     <script src="assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
