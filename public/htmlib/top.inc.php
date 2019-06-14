4fd8be123f293539519c3d040748f648f483b78d
lcodo <laia.codo@bsc.es>
2019-06-14 12:59:54 +0200

    fix main logo framing

--- public/htmlib/top.inc.php
+++ public/htmlib/top.inc.php
@@ -27,8 +27,8 @@ if (file_exists($avatarImg)) {
         <!-- BEGIN LOGO -->
         <div class="page-logo">
             <a href="workspace/">
-                <img src="assets/layouts/layout/img/logo.png" alt="logo" class="logo-default" style="width:35%"/>
-                <img src="assets/layouts/layout/img/VRE.png" alt="logo" class="logo-default"/>
+                <!-- <img src="assets/layouts/layout/img/logo.png" alt="logo" class="logo-default" style="width:35%"/> -->
+                <img src="assets/layouts/layout/img/logoplusvre.png" alt="logo" class="logo-default"/>
             </a>
             <div class="menu-toggler sidebar-toggler">
                 <span></span>
