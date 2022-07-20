d1ec2d478f3129ae00f8fbb6aff882b3d9effcd7
Socayna <57795749+Socayna@users.noreply.github.com>
2022-07-20 10:02:31 +0200

    Update Tooljob.php

--- public/phplib/classes/Tooljob.php
+++ public/phplib/classes/Tooljob.php
@@ -870,11 +870,15 @@ class Tooljob {
                                 " --out_metadata "   .$this->stageout_file_virtual .
 				" --log_file "       .$this->log_file_virtual ;
 	
-	$cmd =  "docker run" .
-		" --privileged -v /var/run/docker.sock:/var/run/docker.sock " .
-		" -u www-data" .
- 		" -v " . $GLOBALS['pubDir']. ":" . $this->pub_dir_virtual  .
- 		" -v " . $GLOBALS['dataDir'].":" . $this->root_dir_virtual .
+	$docker_in_docker_socket="";
+	if (is_file("/.dockerenv") or is_file("/run/.containerenv") ){
+	   $docker_in_docker_socket= " --privileged -v /var/run/docker.sock:/var/run/docker.sock ";
+	}
+#['container_port']	
+	$cmd =  "docker run" . 
+		" --privileged -e ROOT=TRUE -u www-data -v /var/run/docker.sock:/var/run/docker.sock ".
+		" -v " . $this->pub_dir_virtual . ":" . $this->pub_dir_virtual .
+ 		" -v " . $GLOBALS['dataDir']."/".$_SESSION['User']['id'].":" . $this->root_dir_virtual .
  		" ".$tool['infrastructure']['container_image'] . " $cmd_vre";
 
         return $cmd;
