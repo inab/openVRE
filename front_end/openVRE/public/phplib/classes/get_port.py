c6bb0f73cb70808aa2be06e94a4c14e31af0fb27
Nicolás Díaz Roussel <nicolas.diazroussel@bsc.es>
2025-03-07 10:17:13 +0100

    refactor: no need to reuse socket

--- front_end/openVRE/public/phplib/classes/get_port.py
+++ front_end/openVRE/public/phplib/classes/get_port.py
@@ -5,7 +5,6 @@ import sys
 
 def is_port_free(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow socket reuse
         try:
             sock.bind((host, port))
             return True 
