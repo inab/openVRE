71b140d8e497d4d1d4b3bf3fa3e9a239cd18ac78
Nicolás Díaz Roussel <nicolas.diazroussel@bsc.es>
2025-09-23 12:56:40 +0200

    feature: avoid admin user for application queries to DB

--- mongodb/mongo-init.sh
+++ mongodb/mongo-init.sh
@@ -6,21 +6,6 @@ echo ">>>>>>> Starting MongoDB initialization script"
 mongosh <<EOF
 db = db.getSiblingDB('$MONGO_DB');
 
-// Check if the root user exists
-if (db.getUser("$MONGO_INITDB_ROOT_USERNAME") === null) {
-  db.createUser({
-    user: "$MONGO_INITDB_ROOT_USERNAME",
-    pwd: "$MONGO_INITDB_ROOT_PASSWORD",
-    roles: [{
-      role: "dbOwner",
-      db: "$MONGO_DB"
-    }]
-  });
-  print("Root user created.");
-} else {
-  print("Root user already exists.");
-}
-
 // Check if the application user exists
 if (db.getUser("$MONGO_INITDB_USERNAME") === null) {
   db.createUser({
@@ -42,8 +27,7 @@ for mongo_document in /init_documents/*.json; do
     mongoimport --db ${MONGO_DB} \
                 --jsonArray \
                 --port ${MONGO_PORT} \
-                --username ${MONGO_INITDB_ROOT_USERNAME} \
-                --password ${MONGO_INITDB_ROOT_PASSWORD} \
-                --authenticationDatabase admin \
+                --username ${MONGO_INITDB_USERNAME} \
+                --password ${MONGO_INITDB_PASSWORD} \
                 --file $mongo_document
 done
