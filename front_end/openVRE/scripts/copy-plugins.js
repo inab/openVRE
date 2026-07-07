5b1816b0c922677e2e12d9a6a5c87da6b6347737
Osledy Bazó <osledy.bazopaz@bsc.es>
2026-07-07 14:42:32 +0200

    chore: add jquery-validation dependency and update copy-plugins.js
    - Added jquery-validation version 1.14.0 to package.json and package-lock.json.
    - Updated copy-plugins.js to include copying of jquery-validation plugin files to public assets.

--- front_end/openVRE/scripts/copy-plugins.js
+++ front_end/openVRE/scripts/copy-plugins.js
@@ -12,6 +12,10 @@ const COPY_FILES = [
   ['jquery.cookiebar/jquery.cookieBar.min.js', 'jquery-cookiebar/jquery.cookieBar.min.js'],
   ['jquery-blockui/jquery.blockUI.js', 'jquery.blockui.min.js'],
   ['clipboard/dist/clipboard.min.js', 'clipboardjs/clipboard.min.js'],
+  ['jquery-validation/dist/jquery.validate.js', 'jquery-validation/js/jquery.validate.js'],
+  ['jquery-validation/dist/jquery.validate.min.js', 'jquery-validation/js/jquery.validate.min.js'],
+  ['jquery-validation/dist/additional-methods.js', 'jquery-validation/js/additional-methods.js'],
+  ['jquery-validation/dist/additional-methods.min.js', 'jquery-validation/js/additional-methods.min.js'],
 ];
 const COPY_DIRS = [
   ['flot', 'flot', [
@@ -43,7 +47,8 @@ const COPY_DIRS = [
     'scss'
   ]],
   ['select2/dist/js', 'select2/js', ['i18n']],
-  ['select2/dist/css', 'select2/css']
+  ['select2/dist/css', 'select2/css'],
+  ['jquery-validation/dist/localization', 'jquery-validation/js/localization']
 ];
 
 function nm(...parts) {
