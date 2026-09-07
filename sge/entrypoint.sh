f5011d55bf1ffaa660d65f43c94e4e8c49eb2839
Nicolás Díaz Roussel <nicolas.diazroussel@bsc.es>
2026-09-07 17:47:53 +0200

    fix: avoid error with sge user and docker group due to already used id

--- sge/entrypoint.sh
+++ sge/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+SOCKET=/var/run/docker.sock
+if [ -S "$SOCKET" ]; then
+    echo "Adding application user to docker group"
+    SOCKET_GID=$(stat -c '%g' "$SOCKET")
+    EXISTING_GROUP=$(getent group "$SOCKET_GID" | cut -d: -f1 || true)
+
+    if [ -n "$EXISTING_GROUP" ]; then
+        usermod -aG "$EXISTING_GROUP" application
+    else
+        groupadd -g "$SOCKET_GID" dockerhost
+        usermod -aG dockerhost application
+    fi
+fi
+
 mkdir -p /shared_data
 mkdir -p /shared_data/public
 mkdir -p /shared_data/userdata
